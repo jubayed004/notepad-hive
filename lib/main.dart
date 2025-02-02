996feb82ea4ede3450ddc37753d0c8a3139ef6a4
jubayed004 <mohammadjubayed.islam97@gmail.com>
2025-02-02 23:44:49 +0600

    all Project done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'view/screen/notepad_screen.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+
+ await Hive.initFlutter();
+
+
   Box box = await Hive.openBox('notepad');
   runApp(const MyApp());
 }
--- lib/view/screen/notepad_screen.dart
+++ lib/view/screen/notepad_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 class NotepadScreen extends StatefulWidget {
   const NotepadScreen({super.key});
@@ -14,55 +15,120 @@ class _NotepadScreenState extends State<NotepadScreen> {
 
   @override
   void initState() {
-   notepad = Hive.box("notepad");
+    notepad = Hive.box("notepad");
     super.initState();
   }
+
   TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                  hintText: "writing something",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide:
-                          BorderSide(width: 2, color: Colors.greenAccent))),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                    onPressed: () async{
-                          try{
-                            final userInput = _controller.text;
-                            await notepad!.add(userInput);
-                            Fluttertoast.showToast(msg: 'Added Successfully');
-                          }catch(e){
-                            Fluttertoast.showToast(msg: e.toString());
-                          }
-                    },
-                    child: Text(
-                      "Add New Data",
-                      style: TextStyle(color: Colors.white),
-                    ))),
-            Expanded(
-                child: ListView.builder(
-                  itemCount: 16,
-              itemBuilder: (BuildContext context, int index) {
-               return ListTile();
-              },
-            ))
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.import_contacts),
+                    suffixIcon: Icon(Icons.insert_comment_sharp),
+                    hintText: "writing something",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.greenAccent))),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green),
+                      onPressed: () async {
+                        try {
+                          final userInput = _controller.text;
+                          await notepad!.add(userInput);
+                          _controller.clear();
+                          Fluttertoast.showToast(msg: 'Added Successfully');
+                        } catch (e) {
+                          Fluttertoast.showToast(msg: e.toString());
+                        }
+                      },
+                      child: Text(
+                        "Add New Data",
+                        style: TextStyle(color: Colors.white),
+                      ))),
+              Expanded(
+                  child: ValueListenableBuilder(
+                      valueListenable: Hive.box('notepad').listenable(),
+                      builder: (context, box, widget) {
+                        return ListView.builder(
+                          itemCount: notepad!.keys.toList().length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Card(
+                              elevation: 10,
+                              child: ListTile(
+                                textColor: Colors.pink,
+                                title: Text(notepad!.getAt(index).toString()),
+                                trailing: SizedBox(
+                                  width: 100,
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (context) {
+                                                return Dialog(
+                                                  child: Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            20.0),
+                                                    child: Column(
+                                                      children: [
+                                                        TextField(
+                                                          decoration:
+                                                              InputDecoration(
+                                                                  hintText:
+                                                                      "Inter update  data"),
+                                                        ),
+                                                        ElevatedButton(
+                                                            onPressed: () {},
+                                                            child:
+                                                                Text("Update"))
+                                                      ],
+                                                    ),
+                                                  ),
+                                                );
+                                              });
+                                        },
+                                        icon: Icon(
+                                          Icons.edit,
+                                          color: Colors.purple,
+                                        ),
+                                      ),
+                                      IconButton(
+                                          onPressed: () async {
+                                           await notepad!.deleteAt(index);
+                                           Fluttertoast.showToast(msg: 'Deleted Successfully');
+                                          },
+                                          icon: Icon(
+                                            Icons.delete,
+                                            color: Colors.red,
+                                          ))
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }))
+            ],
+          ),
         ),
       ),
     );
